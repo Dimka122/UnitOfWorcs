d9a0a077af6fb3ed643629d0b208545df7411849
Дмитрий Кассин <kassindima1@gmail.com>
2022-11-21 17:55:46 +0200

    aaa

--- OfWorcs/OfWorcs/Models/UnitOfWorks/Admin/AdminRepo.cs
+++ OfWorcs/OfWorcs/Models/UnitOfWorks/Admin/AdminRepo.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+namespace OfWorcs.Models.UnitOfWorks.Admin
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    public class AdminRepo : IAdminRepo<Product>
+    {
+        private ProductContext context;
+        public AdminRepo(ProductContext context)
+        {
+            this.context = context;
+        }
+        public Product Get(int id) => context.Products.Find(id);
+        public IEnumerable<Product> GetAll() => context.Products;
+        public IDisposable Add(Product entity)
+        {
+            context.Products.Add(entity);
+            return (IDisposable)this;
+        }
+        public IDisposable Update(Product entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            return (IDisposable)this;
+        }
+        public IDisposable Delete(Product entity)
+        {
+            context.Products.Remove(entity);
+            return (IDisposable)this;
+        }
+    }
 }
